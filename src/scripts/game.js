bab1e6ce916426041f80db2e5bdc35edd4d23032
abaez8031 <abaez8031@gmail.com>
2023-03-10 22:29:06 -0500

    stopped shooting with no time

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -13,47 +13,65 @@ class Game {
     this.stage = new Stage(ctx);
     this.timeRemaining = 60;
     this.score = 0;
+    this.playing = true;
     canvas.addEventListener("click", this.handleClick.bind(this));
-    // setInterval(this.renderScore.bind(this), 0);
-    // setInterval(() => {
-    //   this.timeRemaining--;
-    // }, 1000);
+    setInterval(this.renderScore.bind(this), 0);
+    const timer = setInterval(() => {
+      this.timeRemaining--;
+      if (this.timeRemaining <= 0) {
+        this.endGame()
+      }
+    }, 1000);
   }
 
   handleClick(e) {
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    this.stage.worms.push(new Worm(this.ctx, x, y));
-    this.stage.worms.forEach((worm) => this.checkWormShot(worm));
-    setTimeout(() => {
-      this.stage.worms = [];
-    }, 300);
+    if (this.playing) {
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      this.stage.worms.push(new Worm(this.ctx, x, y));
+      this.stage.worms.forEach((worm) => this.checkWormShot(worm));
+      setTimeout(() => {
+        this.stage.worms = [];
+      }, 300);
+    } else {
+      return;
+    }
   }
 
   checkWormShot(worm) {
-    this.stage.birds.forEach((bird) => {
-      if (
-        worm.x < bird.x + bird.width &&
-        worm.x + worm.width > bird.x &&
-        worm.y < bird.y + bird.height &&
-        worm.y + worm.height > bird.y
-      ) {
-        console.log("HIT");
-        const index = this.stage.birds.indexOf(bird);
-        this.stage.birds.splice(index, 1);
-        this.addPoint.bind(this)();
-      }
-    });
+    if (this.playing) {
+      this.stage.birds.forEach((bird) => {
+        if (
+          worm.x < bird.x + bird.width &&
+          worm.x + worm.width > bird.x &&
+          worm.y < bird.y + bird.height &&
+          worm.y + worm.height > bird.y
+        ) {
+          console.log("HIT");
+          const index = this.stage.birds.indexOf(bird);
+          this.stage.birds.splice(index, 1);
+          this.addPoint.bind(this)();
+        }
+      });
+    } else {
+      return;
+    }
   }
 
   addPoint() {
     this.score++;
   }
 
-  // renderScore() {
-  //   this.ctx.fillText(`SCORE: ${this.score} TIME: ${this.timeRemaining}`);
-  // }
+  renderScore() {
+    const scoreboard = document.getElementById("scoreboard");
+    scoreboard.innerText = `SCORE: ${this.score} TIME: ${this.timeRemaining}`;
+  }
+
+  endGame() {
+    this.playing = false;
+    clearInterval(timer);
+  }
 }
 
 new Game();
